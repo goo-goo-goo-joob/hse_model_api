023c1f7f91a30ef298cf78a6f0aff7fecbe925c9
goo-goo-goo-joob <pro1895sto@gmail.com>
2022-10-28 20:49:53 +0000

    refactoring

--- model_api/__main__.py
+++ model_api/__main__.py
@@ -1,10 +1,10 @@
-from model_api.controller import app
-
 import argparse
 
+from model_api.controller import app
+
 parser = argparse.ArgumentParser()
-parser.add_argument('port', type=int, help='port')
-parser.add_argument('host', type=str, help='host')
+parser.add_argument('--port', type=int, help='port')
+parser.add_argument('--host', type=str, help='host')
 
 if __name__ == "__main__":
     args = parser.parse_args()
