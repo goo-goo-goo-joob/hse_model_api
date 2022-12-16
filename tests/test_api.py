8898ecd4637c2809e68f295bc442ccdb8444b012
goo-goo-goo-joob <pro1895sto@gmail.com>
2022-12-16 22:02:24 +0000

    add mock tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,12 @@
-from model_api.models import CatBoostClassifierModel, CatBoostRegressorModel
+import psycopg2
+
 from sklearn.datasets import make_classification, make_regression
 from sklearn.metrics import mean_squared_error
 import pytest
+
+from model_api.models import CatBoostClassifierModel, CatBoostRegressorModel, ModelTypes
+from model_api.service import Service
+from model_api.database import DataBase
 
 
 def test_simple():
@@ -15,7 +20,7 @@ def test_simple():
      {"iterations": 100, "max_depth": 3, "random_state": 42, "logging_level": "Silent"}, 400),
 
 ])
-def test_classification(generator, cls, params, err):
+def test_model(generator, cls, params, err):
     X, y = generator(n_samples=10_000, random_state=42)
     model = cls(params)
     model.fit(X, y)
@@ -25,3 +30,22 @@ def test_classification(generator, cls, params, err):
     assert isinstance(model2, cls)
     proba2 = model2.predict(X)
     assert mean_squared_error(proba, proba2) * len(proba) < 10 ** -6
+
+
+@pytest.mark.parametrize("generator,model_type,params", [
+    (make_classification, "catboost_classifier",
+     {"iterations": 100, "max_depth": 3, "random_state": 42, "logging_level": "Silent"}),
+    (make_regression, "catboost_regressor",
+     {"iterations": 100, "max_depth": 3, "random_state": 42, "logging_level": "Silent"}),
+
+])
+def test_db(mocker, generator, model_type, params):
+    mocker.patch("psycopg2.connect")
+    mocker.patch.object(DataBase, "create_model")
+    mocker.patch.object(DataBase, "get_model", {"id": 0, "type": model_type, "params": params, "binary": b""})
+    s = Service()
+    psycopg2.connect.assert_called_once()
+    X, y = generator(n_samples=10_000, random_state=42)
+    s.model_train(X, y, model_type, params)
+    DataBase.create_model.assert_called_once()
+    assert set(s.get_model_list()) == set(ModelTypes.keys())
